aa0b8c1ff8ec9d816cda2d1b955a0f8b54877eb8
Aykut Kardaş <aykutkrds@gmail.com>
2020-01-07 14:17:56 +0300

    Changed default index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,14 @@ import { inject, observer } from 'mobx-react'
 @inject('store')
 @observer
 class HomePage extends React.Component {
-  render(){
-  return <div className="text-danger">Welcome to Next.js!</div>
+  render() {
+    return <div className="container">
+      <div className="row">
+        <div className="col-4 mx-auto text-primary">
+          Hello, Index Page!
+        </div>
+      </div>
+    </div>
   }
 }
 
